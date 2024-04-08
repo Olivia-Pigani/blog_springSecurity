b7a3a23cacdceb1bb63672bafb65f9e5d7edf542
Olivia-Pigani <piganiolivia@gmail.com>
2024-04-08 22:56:15 +0200

    commit

--- src/main/java/com/example/blog_spring_mvc/controller/AuthenticationController.java
+++ src/main/java/com/example/blog_spring_mvc/controller/AuthenticationController.java
@@ -1,56 +1,62 @@
 package com.example.blog_spring_mvc.controller;
 
+import com.example.blog_spring_mvc.dto.UserSignInDTO;
+import com.example.blog_spring_mvc.dto.UserSignUpDTO;
+import com.example.blog_spring_mvc.entity.User;
 import com.example.blog_spring_mvc.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("auth")
 public class AuthenticationController {
 
+    private final AuthenticationManager authenticationManager;
 
+    private final PasswordEncoder passwordEncoder;
     private final UserService userService;
 
     @Autowired
-    public AuthenticationController(UserService userService) {
+    public AuthenticationController(AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
         this.userService = userService;
     }
 
-//    @GetMapping("/auth-form")
-//    public String getAuthForm(Model model) {
-////        model.addAttribute()
-//        return "auth-form";
-//    }
-//
-//
-//    @PostMapping("/signup")
-//    public ResponseEntity<String> registerUser(@RequestBody User newUser){
-//       return userService.saveUser(newUser);
-//    }
-//
-//    @PostMapping("/signin")
-//    public String signIn(@ModelAttribute("userSignInDTO") UserSignInDTO userSignInDTO, Model model, HttpSession session) {
-//        if (userService.findUserByEmail(userSignInDTO.getEmail()).isPresent()) {
-//            if (userService.verifyUser(userSignInDTO.getEmail(), userSignInDTO.getPassword())) {
-//                Map<String, Object> data = new HashMap<>();
-//                data.put("token", userService.generateToken(userSignInDTO.getEmail(), userSignInDTO.getPassword()));
-//                new BaseResponseDTO("success", data);
-//                session.setAttribute("isLogged", true);
-//                return "redirect:/";
-//            } else {
-//                new BaseResponseDTO("wrong password");
-//                return "redirect:/";
-//            }
-//
-//        } else {
-//            new BaseResponseDTO("user not exist");
-//            session.setAttribute("isLogged", false);
-//            return "auth-form";
-//        }
-//
-//    }
-//
+@PostMapping("/signup")
+public ResponseEntity<String> signUp(@Valid @RequestBody UserSignUpDTO userSignUpDTO) {
+    if (userService.findUserByEmail(userSignUpDTO.getEmail()).isPresent()) {
+        return new ResponseEntity<>("this email already exists", HttpStatus.BAD_REQUEST);
+    }
 
+    User user = User.builder()
+            .name(userSignUpDTO.getName())
+            .email(userSignUpDTO.getEmail())
+            .password(passwordEncoder.encode(userSignUpDTO.getPassword()))
+            .build();
 
+    userService.saveUser(user);
+
+    return new ResponseEntity<>("success, this user is now in our database !", HttpStatus.OK);
+
+}
+    @PostMapping("/signin")
+    public ResponseEntity<String> signIn(@Valid @RequestBody UserSignInDTO userSignInDTO) {
+        String token = userService.generateToken(userSignInDTO.getEmail(), userSignInDTO.getPassword());
+        if (token == null) {
+            return new ResponseEntity<>("there is an error about email and/or password !", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Bearer " + token, HttpStatus.OK);
+    }
 // no sign out in here, we'll manage this one in thymeleaf html files
 
 
--- src/main/java/com/example/blog_spring_mvc/dto/UserSignUpDTO.java
+++ src/main/java/com/example/blog_spring_mvc/dto/UserSignUpDTO.java
@@ -5,9 +5,13 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Getter
+@Setter
 public class UserSignUpDTO {
 
     @NotBlank
